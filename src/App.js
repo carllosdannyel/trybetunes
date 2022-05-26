2a6629e2a98dce18bcafb610c960156141c4ac55
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-25 22:18:48 -0300

    corrige erro da 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 
 class App extends React.Component {
   render() {
@@ -20,10 +19,8 @@ class App extends React.Component {
             <Route path="/search" component={ Search } />
             <Route path="/album" component={ Album } />
             <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } />
-            <Route path="/profile/edit" component={ ProfileEdit }>
-              <Header />
-            </Route>
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
             <Route path="*" component={ NotFound } />
           </Switch>
         </BrowserRouter>
